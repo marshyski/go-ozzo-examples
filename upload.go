5164720f0c250c0c94705812411d023cafe6af61
hla537 <timothy.marcinowski@capitalone.com>
2016-04-28 16:17:16 -0400

    Added upload example

--- upload.go
+++ upload.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "log"
-  "net/http"
-  "io"
-  "github.com/go-ozzo/ozzo-routing"
-  "github.com/go-ozzo/ozzo-routing/fault"
-  "github.com/go-ozzo/ozzo-routing/slash"
-  "github.com/go-ozzo/ozzo-routing/access"
-  "github.com/go-ozzo/ozzo-routing/content"
+	"fmt"
+	"github.com/go-ozzo/ozzo-routing"
+	"github.com/go-ozzo/ozzo-routing/access"
+	"github.com/go-ozzo/ozzo-routing/content"
+	"github.com/go-ozzo/ozzo-routing/fault"
+	"github.com/go-ozzo/ozzo-routing/slash"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
@@ -24,26 +24,26 @@ func main() {
 	)
 
 	api := router.Group("/v1")
-  api.Use(
-      content.TypeNegotiator(content.JSON),
-  )
+	api.Use(
+		content.TypeNegotiator(content.JSON),
+	)
 
 	api.Post("/upload", func(c *routing.Context) error {
 
-    c.Request.ParseMultipartForm(32 << 20)
-    file, handler, err := c.Request.FormFile("file")
-    if err != nil {
-        fmt.Println(err)
-        return nil
-    }
-    defer file.Close()
-    f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0664)
-    if err != nil {
-        fmt.Println(err)
-        return nil
-    }
-    defer f.Close()
-    io.Copy(f, file)
+		c.Request.ParseMultipartForm(32 << 20)
+		file, handler, err := c.Request.FormFile("file")
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		defer file.Close()
+		f, err := os.OpenFile(handler.Filename, os.O_WRONLY|os.O_CREATE, 0664)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		defer f.Close()
+		io.Copy(f, file)
 
 		return c.Write(handler.Filename)
 	})
